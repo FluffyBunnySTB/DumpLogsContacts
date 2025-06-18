05886807e77e4fa7f135ec05bf96733cd60ed091
Gerhardus <gerhardus@rocketmail.com>
2025-06-18 11:06:46 +0200

    Added strings to buttons and messed arround getting the build to release package to work.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,26 +4,34 @@ plugins {
 }
 
 android {
-    namespace = "net.za.digiscan.dumpcontact"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "net.za.digiscan.dumpcontact"
-        minSdk = 21
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
     signingConfigs {
-        create("release") {
-            storeFile = file("C:/Users/schelteg/Documents/OneDrive - gerhardus03/OneDrive/Documents/Keys/AndroidDevelopmentKeystore.jks")
+        getByName("debug") {
+            storeFile =
+                file("C:\\Users\\schelteg\\Documents\\OneDrive - gerhardus03\\OneDrive\\Documents\\Keys\\AndroidDevelopmentKeystore.jks")
             storePassword = "Ul47sjYXanQru4i8ouL80q7QwwXIJl"
             keyAlias = "LogDump"
             keyPassword = "JLf4yCQMNgMgtpKD4pA2Ch8vZjmzoA"
         }
+        create("release") {
+            storeFile =
+                file("C:\\Users\\schelteg\\Documents\\OneDrive - gerhardus03\\OneDrive\\Documents\\Keys\\AndroidDevelopmentKeystore.jks")
+            storePassword = "Ul47sjYXanQru4i8ouL80q7QwwXIJl"
+            keyAlias = "LogDump"
+            keyPassword = "JLf4yCQMNgMgtpKD4pA2Ch8vZjmzoA"
+        }
+    }
+    namespace = "net.za.digiscan.dumpcontact"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.altron.dumpcontact"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        signingConfig = signingConfigs.getByName("release")
     }
 
     buildTypes {
@@ -33,6 +41,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
--- app/src/androidTest/java/net/za/digiscan/dumpcontact/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/net/za/digiscan/dumpcontact/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.altron.dumpcontact
+package net.za.digiscan.dumpcontact
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.altron.dumpcontact", appContext.packageName)
+        assertEquals("net.za.digiscan.dumpcontact", appContext.packageName)
     }
 }
